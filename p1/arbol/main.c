67a3611bba8d631d75e5636d6ed4237a02993823
roxana <roxy.hm98@gmail.com>
2020-10-28 21:47:22 -0600

    Se desaloja el árbol después del ordenamiento
    Se modificó el archivo "main.c" de tal manera que se liberara
    la memoria del árbol utilizado para hacer el ordenamiento sin afectar
    las mediciones de tiempo del algoritmo.

--- p1/arbol/main.c
+++ p1/arbol/main.c
@@ -4,24 +4,14 @@
 #include "../utils/util.h"
 #include "../utils/tiempo.h"
 
-void ordena_con_arbol_binario_busqueda(int *arreglo, int n)
-{
-	int i;
-	ArbinOr arbol = inicArbinOr();
-
-	for (i = 0; i < n; i++)
-	{
-		arbol = insArbinOr(arbol, arreglo[i]);
-	}
-
-	guardar_recorrido_inorder(arbol, arreglo);
-}
+void ordena_con_arbol_binario_busqueda(ArbinOr arbol, int *arreglo, int n);
 
 int main(int argc, const char **argv)
 {
-	double utime0, stime0, wtime0, utime1, stime1, wtime1;
+	double utime0, stime0, wtime0, utime1, stime1, wtime1;	
 	int n, i;
 	int *arreglo;
+	ArbinOr arbol = inicArbinOr();
 
 	if (argc == 2)
 	{
@@ -36,7 +26,7 @@ int main(int argc, const char **argv)
 
 	uswtime(&utime0, &stime0, &wtime0);
 
-	ordena_con_arbol_binario_busqueda(arreglo, n);
+	ordena_con_arbol_binario_busqueda(arbol, arreglo, n);
 
 	uswtime(&utime1, &stime1, &wtime1);
 	
@@ -45,5 +35,19 @@ int main(int argc, const char **argv)
 	imprimir_arreglo(arreglo, n);
 	printf("\n");
 
+	destruirArbinOr(arbol); 
+
 	return 0;
+}
+
+void ordena_con_arbol_binario_busqueda(ArbinOr arbol, int *arreglo, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		arbol = insArbinOr(arbol, arreglo[i]);
+	}
+
+	guardar_recorrido_inorder(arbol, arreglo);
 }
